a3b85788c7a4c35b504153810c8528c05afd8aab
Saurabh <bhsaurabh@users.noreply.github.com>
2025-01-28 11:43:10 +0100

    Complex state 2

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -20,12 +20,14 @@ const App = () => {
   // useState creates the state, and returns current state and method to change state
   // method to adapt state will re-render the component since state is changed.
   const [counter, setCounter] = useState(0);  // 0 is the default value of state passed to be assigned.
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  const [clicks, setClicks] = useState( { left: 0, right: 0} );
 
   const increaseByOne = () => setCounter(counter+1);
 
   const decreaseByOne = () => setCounter(counter-1);
+
+  const handleLeftClick = () => setClicks({ left: clicks.left+1, right: clicks.right });
+  const handleRightClick = () => setClicks({ left: clicks.left, right: clicks.right+1 });
 
   const setToZero = () => setCounter(0);
 
@@ -35,12 +37,11 @@ const App = () => {
       <Button onClick={increaseByOne} text="plus" />
       <Button onClick={setToZero} text="zero" />
       <Button onClick={decreaseByOne} text="minus" />
-
-      <Button onClick = { () => setLeft(left+1) } text="left" />
-      <Button onClick = { () => setRight(right+1) } text="right" />
       <br />
-      <Display counter={left} />
-      <Display counter={right} />
+      <Button onClick={handleLeftClick} text="left" />
+      <Button onClick={handleRightClick} text="right" />
+      <Display counter={clicks.left} />
+      <Display counter={clicks.right} />
     </div>
   );
 
