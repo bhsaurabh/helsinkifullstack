68c8e2e07d518dea71416dd2bad2c4b6ddcd2769
Saurabh <bhsaurabh@users.noreply.github.com>
2025-01-28 17:08:37 +0100

    Add some work about rendering collections

---
+++ part2/introdemo2/src/App.jsx
@@ -0,0 +1,20 @@
+const Note = ({note}) => {
+  return (
+    <li>{note.content}</li>
+  );
+}
+
+
+const App = ({notes}) => {
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        { notes.map(note => <Note note={note} key={note.id} />) }
+      </ul>
+    </div>
+  )
+}
+
+export default App
