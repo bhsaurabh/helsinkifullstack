8e431358706e30db6dc610e053ab8bc93721faaf
Saurabh <bhsaurabh@users.noreply.github.com>
2025-01-28 11:39:35 +0100

    Complex state 1

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -20,6 +20,8 @@ const App = () => {
   // useState creates the state, and returns current state and method to change state
   // method to adapt state will re-render the component since state is changed.
   const [counter, setCounter] = useState(0);  // 0 is the default value of state passed to be assigned.
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
 
   const increaseByOne = () => setCounter(counter+1);
 
@@ -33,6 +35,12 @@ const App = () => {
       <Button onClick={increaseByOne} text="plus" />
       <Button onClick={setToZero} text="zero" />
       <Button onClick={decreaseByOne} text="minus" />
+
+      <Button onClick = { () => setLeft(left+1) } text="left" />
+      <Button onClick = { () => setRight(right+1) } text="right" />
+      <br />
+      <Display counter={left} />
+      <Display counter={right} />
     </div>
   );
 
