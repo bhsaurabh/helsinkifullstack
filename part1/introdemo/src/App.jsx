6fa33012e6ae9f2cfcecd1886a6fa39fe6b7d383
Saurabh <bhsaurabh@users.noreply.github.com>
2025-01-28 11:02:03 +0100

    Modularise better by passing state to child components

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,6 +1,21 @@
 import { useState } from 'react'
 
 
+const Display = ({counter}) => {
+  return (
+    <div>{counter}</div>
+  );
+}
+
+
+const Button = ({onClick, text}) => {
+  return(
+    <button onClick={ onClick }>{text}</button>
+  );
+}
+
+
+
 const App = () => {
   // useState creates the state, and returns current state and method to change state
   // method to adapt state will re-render the component since state is changed.
@@ -8,13 +23,16 @@ const App = () => {
 
   const increaseByOne = () => setCounter(counter+1);
 
+  const decreaseByOne = () => setCounter(counter-1);
+
   const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={ increaseByOne }>plus</button>
-      <button onClick={ setToZero }>zero</button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   );
 
