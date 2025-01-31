417b6025b1ee2c2bff9582fc86cb95b1a9f6190a
Saurabh <bhsaurabh@users.noreply.github.com>
2025-01-31 07:41:05 +0100

    Confirm if person truly should be deleted

--- part2/phonebook/src/components/ListPersons.jsx
+++ part2/phonebook/src/components/ListPersons.jsx
@@ -3,13 +3,20 @@ import Button from './Button'
 import personsService from '../services/persons'
 
 const handleDelete = (person, persons, setPersons) => {
-  // delete person from the DB
-  personsService
-    .deletePerson(person.id)
-    .then(response => {
-        // update the state and remove the person from the list
-        setPersons(persons.filter(p => p.id !== person.id))
-    })
+    // use a window.confirm to ask the user if they are sure they want to delete the person
+    if (window.confirm(`Delete ${person.name}?`)) {
+        // delete person from the DB
+        personsService
+            .deletePerson(person.id)
+            .then(response => {
+                // update the state and remove the person from the list
+                setPersons(persons.filter(p => p.id !== person.id))
+            })
+            .catch(error => {
+                console.log(error)
+                alert('error deleting person')
+            })
+    }
 }
 
 
