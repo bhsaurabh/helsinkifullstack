899e8b5f8e86d7c12e167f35d138937c3272a680
saurabh <saurabhatia@protonmail.com>
2025-02-07 11:15:04 +0100

    A script to explore mongoose, and help create the schema I need

---
+++ part3/phonebook_be/mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    // password has not been provided as an arg.
+    console.log('give password as an argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]  // 1st is node exec, then is the current script's exec and then are args
+
+const url = `mongodb+srv://fullstack:${password}@fsdev.t0c97.mongodb.net/phonebook?retryWrites=true&w=majority&appName=FSDev`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: Number,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: 'Tester Bester',
+    number: 123
+})
+
+person.save().then(result => {
+    console.log('person saved', person)
+    mongoose.connection.close()
+})
